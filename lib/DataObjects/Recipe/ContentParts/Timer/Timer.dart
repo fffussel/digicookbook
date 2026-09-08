3c63c804484b53d8851f43f238f8f4680666179c
Lucas Schladitz <lsl@hup.de>
2026-09-08 09:04:12 +0200

    test with timer

--- lib/DataObjects/Recipe/ContentParts/Timer/Timer.dart
+++ lib/DataObjects/Recipe/ContentParts/Timer/Timer.dart
@@ -3,4 +3,14 @@ import 'package:digicookbook/DataObjects/Recipe/ContentParts/contentpart.dart';
 
 class Timer extends Contentpart {
   double timeInMinutes = 0;
+
+  @override
+  String toJSON() {
+    var minutes = this.timeInMinutes;
+    return '''
+      'Timer': {
+        'timeInMinutes': $minutes
+       }
+    ''';
+  }
 }
