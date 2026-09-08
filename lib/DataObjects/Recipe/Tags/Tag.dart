7596cd2e5a8c8c77664b40f9cb3e832ec7565d1e
Lucas Schladitz <lsl@hup.de>
2026-09-08 08:46:56 +0200

    tags

---
+++ lib/DataObjects/Recipe/Tags/Tag.dart
@@ -0,0 +1,7 @@
+
+class Tag {
+  String name;
+  String normName;
+
+  Tag(this.name, this.normName);
+}
--- lib/DataObjects/Recipe/recipee.dart
+++ lib/DataObjects/Recipe/recipee.dart
@@ -3,13 +3,15 @@
 import 'dart:io';
 
 import 'package:digicookbook/DataObjects/Recipe/ContentParts/contentpart.dart';
+import 'package:digicookbook/DataObjects/Recipe/Tags/Tag.dart';
 
 class Recipe {
   late String id;
   late String name;
   late String description;
   late File image;
+  late List<Tag> tags;
   late List<Contentpart> parts;
 
-  Recipe (this.id, this.name, this.description, this.image, this.parts);
+  Recipe (this.id, this.name, this.description, this.image, this.tags, this.parts);
 }
